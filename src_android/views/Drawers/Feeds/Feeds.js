022a9cee58598edac6663c199efd7e58524a65db
aeflorez9707 <andresflorez9707@gmail.com>
2019-05-09 16:34:42 -0500

    Más avances

--- src_android/views/Drawers/Feeds/Feeds.js
+++ src_android/views/Drawers/Feeds/Feeds.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { View, ActivityIndicator, Image,TouchableOpacity } from 'react-native';
+import { View, ActivityIndicator, Image,TouchableOpacity,SafeAreaView,Platform,ScrollView,
+  Dimensions,StatusBar,TextInput } from 'react-native';
 import { Text, Content, Card, CardItem, Thumbnail, Left, Body, Right,Button,Icon } from 'native-base';
 import styles from '../../../../assets/styles/styles';
 import { Actions } from 'react-native-router-flux';
 import HeaderDefault from '../../../components/Header/HeaderDefault';
 import axios from 'axios';
-
+import Category from '../../../components/Explore/Category'
+const { height, width } = Dimensions.get('window')
 export default class Feeds extends React.Component {
 
   constructor(props) {
@@ -16,14 +18,68 @@ export default class Feeds extends React.Component {
       data: []
     }
   }
-
-
+  componentWillMount() {
+       this.startHeaderHeight = 80
+       if (Platform.OS == 'android') {
+           this.startHeaderHeight = 100 + StatusBar.currentHeight
+       }
+   }
 
   render() {
     return (
       <View style={styles.drawerContainer}>
         <HeaderDefault title="Menú principal"/>
         <Content padder>
+
+        <SafeAreaView style={{ flex: 1 }}>
+                 <View style={{ flex: 1 }}>
+                     
+                     <ScrollView
+                         scrollEventThrottle={16}
+                     >
+                         <View style={{ flex: 1, backgroundColor: 'white', paddingTop: 20 }}>
+                             <Text style={{ fontSize: 24, fontWeight: '700', paddingHorizontal: 20 }}>
+                                 What can we help you find, Varun?
+                             </Text>
+
+                             <View style={{ height: 130, marginTop: 20 }}>
+                                 <ScrollView
+                                     horizontal={true}
+                                     showsHorizontalScrollIndicator={false}
+                                 >
+                                     <Category imageUri={require('../../../../assets/images/activity.jpeg')}
+                                         name="Home"
+                                     />
+                                     <Category imageUri={require('../../../../assets/images/activity.jpeg')}
+                                         name="Experiences"
+                                     />
+                                     <Category imageUri={require('../../../../assets/images/activity.jpeg')}
+                                         name="Resturant"
+                                     />
+                                 </ScrollView>
+                             </View>
+                             <View style={{ marginTop: 40, paddingHorizontal: 20 }}>
+                                 <Text style={{ fontSize: 24, fontWeight: '700' }}>
+                                     Introducing Airbnb Plus
+                                 </Text>
+                                 <Text style={{ fontWeight: '100', marginTop: 10 }}>
+                                     A new selection of homes verified for quality & comfort
+
+                                 </Text>
+                                 <View style={{ width: width - 40, height: 200, marginTop: 20 }}>
+                                     <Image
+                                         style={{ flex: 1, height: null, width: null, resizeMode: 'cover', borderRadius: 5, borderWidth: 1, borderColor: '#dddddd' }}
+                                         source={require('../../../../assets/images/activity.jpeg')}
+                                     />
+
+                                 </View>
+                             </View>
+                         </View>
+                     </ScrollView>
+
+                 </View>
+ </SafeAreaView>
+
 
 
         <TouchableOpacity onPress={() => Actions.desarrollo( Actions.midesarrollo())}>
